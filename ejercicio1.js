a293a7b5d24b098e74fd34c244a7affb17bd8c68
emasalpe <e.salpetri@gmail.com>
2024-10-02 23:20:05 -0300

    ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,14 @@
+// Crea un array con elementos
+let array = [5, 2, 8, 1, 9];
+console.log(array); // [5, 2, 8, 1, 9]
+// Eliminar primer valor
+array.shift();
+console.log(array); // [2, 8, 1, 9]
+// Eliminar ultimo valor
+array.pop();
+console.log(array); // [2, 8, 1]
+// Mostrar el indice 2
+console.log(array[2]); // 1
+// Mostrar indexof
+console.log(array)
+console.log(`En el indice nro 1 se encuentra el valor: ${array.indexOf(1)}`); // 1
